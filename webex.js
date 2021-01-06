df47a82453fef2bbed4db3dfd9e7cb82a362742d
ethandchen <68520785+ethandchen@users.noreply.github.com>
2021-01-05 17:39:18 -0800

    Add files via upload

--- webex.js
+++ webex.js
@@ -10,29 +10,36 @@ scheduled_flights = [];
 const Stream = require("stream").Transform;
 const fs = require("fs");
 
+//Schedules a flight
+let ID = "N433TM";
+let est_dep = 1609715691;
+scheduleFlight(ID, est_dep);
 
+//print out the flights that have been scheduled
+for(let a = 0; a < scheduled_flights.length; a++) {
+    console.log(scheduled_flights[a]);
+}
 
+//Creates flight object given a flight ID and an estimated depature time in UNIX time
+//Adds flight object to array of flights
+function scheduleFlight(ID_in, est_dep) {
 
-findFlightByIDandTime("N433TM  ", 1609715691);
+    let flight = {
+        ID: ID_in,
+        departure: est_dep
+    }
 
-
-
-
-  function findFlightByIDandTime(ID, est_dep) {
+    let found = false;
 
     let begin =  est_dep;
     let end = est_dep + 7200;
 
     //search through api for the flight
-
     let url = "https://opensky-network.org/api/flights/all?";
 
     url += ("begin=" + begin);
     url += "&";
     url += ("end=" + end);
-
-    
-
 
     https
       .get(url, resp => {
@@ -41,19 +48,16 @@ findFlightByIDandTime("N433TM  ", 1609715691);
         // A chunk of data has been recieved.
         resp.on("data", chunk => {
           data += chunk;
-          //console.log(data.substring(0, 200));
         });
 
         // The whole response has been received. Print out the result.
         resp.on("end", () => {
           let parsed = JSON.parse(data);
-          // console.log(parsed.length);
           
           //loop through the list of api flights, search for the flight ID
-
           for (let i = 0; i < parsed.length; i++) {
             if (parsed[i].callsign == ID) {
-              console.log(parsed[i].firstSeen);
+              found = true;
             }
           }
 
@@ -62,6 +66,12 @@ findFlightByIDandTime("N433TM  ", 1609715691);
         console.log("Error: " + err.message);
       });
 
+      //Add flight object to the array of flights
+      if(found) {
+        scheduled_flights.push(flight);
+      }else {
+        console.log("Flight ID: " + flight.ID + " not found");
+      }
   })
 }
 
