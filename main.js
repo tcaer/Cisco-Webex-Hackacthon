679303e4d5ebf1976caa0de6ba764893f26b95bd
Tino Caer <corentin.caer@applydigital.co>
2021-01-08 13:28:28 -0500

    Add basic express server and bot

--- main.js
+++ main.js
@@ -1,1 +1,60 @@
 require("dotenv").config();
+let Framework = require("webex-node-bot-framework");
+let webhook = require("webex-node-bot-framework/webhook");
+
+let express = require("express");
+let bodyParser = requrie("body-parser");
+
+// The server that will accept webhooks and host the calendar
+var expressApp = express();
+
+// Allow the usage of ?= url params and json bodies
+expressApp.use(bodyParser.urlencoded({extended: true}));
+expressApp.use(bodyParser.json());
+
+const config = {
+  webhookUrl: process.env.BOT_WEBHOOK,
+  token: process.env.BOT_ACCESS_TOKEN,
+  port: 80 // Server runs at :8080
+};
+
+var framework = new Framework(config);
+framework.start();
+
+framework.on("initialized", function () {
+  framework.debug("Framework initialized successfully! [Press CTRL-C to quit]");
+});
+
+framework.on('spawn', function (bot, id, addedBy) {
+  if (!addedBy) {
+    // don't say anything here or your bot's spaces will get 
+    // spammed every time your server is restarted
+    framework.debug(`Framework created an object for an existing bot in a space called: ${bot.room.title}`);
+  } else {
+    // addedBy is the ID of the user who just added our bot to a new space, 
+    // Say hello, and tell users what you do!
+    bot.say('Hi there, you can say hello to me.  Don\'t forget you need to mention me in a group space!');
+  }
+});
+
+/* Server stuff */
+server.post("/webhook", webhook(framework));
+
+var server = app.listen(config.port, function() {
+  framework.debug(`Framework listening on port ${config.port}`);
+});
+
+function exitHandler() {
+  framework.debug("Stopping...");
+  server.close();
+
+  framework.stop().then(function() {
+    process.exit();
+  });
+}
+
+process.on("SIGINT", exitHandler);
+process.on("exit", exitHandler);
+process.on("SIGUSR1", exitHandler);
+process.on("SIGUSR2", exitHandler);
+process.on("uncaughtException", exitHandler);
